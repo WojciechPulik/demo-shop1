f08ddd7b134aba6cf26ca24c5ffadfb7e4a868d6
WojciechPulik <59091416+WojciechPulik@users.noreply.github.com>
2020-08-25 09:34:22 +0200

    Category

--- src/main/java/pl/wpulik/controller/HomeController.java
+++ src/main/java/pl/wpulik/controller/HomeController.java
@@ -1,15 +1,19 @@
 package pl.wpulik.controller;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import pl.wpulik.model.Category;
 import pl.wpulik.model.Picture;
 import pl.wpulik.model.Product;
+import pl.wpulik.service.CategoryRepoService;
 import pl.wpulik.service.PictureRepoService;
 import pl.wpulik.service.ProductRepoService;
 
@@ -18,18 +22,21 @@ public class HomeController {
 	
 	private ProductRepoService productRepoService;
 	private PictureRepoService pictureService;
+	private CategoryRepoService categoryRepoService;
 	
 	@Autowired
-	public HomeController(ProductRepoService productRepoService, PictureRepoService pictureService) {
+	public HomeController(ProductRepoService productRepoService, PictureRepoService pictureService, CategoryRepoService categoryRepoService) {
 		this.productRepoService = productRepoService;
 		this.pictureService = pictureService;
+		this.categoryRepoService = categoryRepoService;
 	}
 	
 	@GetMapping("/")
 	public String home (Model model) {
 		Integer addedQuantity = 0;
 		List<Picture> pictures = new ArrayList<>();
-		List<Product> products = productRepoService.getAllProducts();	
+		List<Product> products = productRepoService.getAllProducts();
+		List<Category> categories = categoryRepoService.getAllCategories();
 		for(Product p: products) {
 			if(!pictureService.getByProductId(p.getId()).isEmpty()) {
 				p.getPictures().add(pictureService.getByProductId(p.getId()).get(0));
@@ -43,6 +50,7 @@ public class HomeController {
 		model.addAttribute("pictures", pictures);
 		model.addAttribute("products", products);
 		model.addAttribute("addedQuantity", addedQuantity);
+		model.addAttribute("categories", categories);
 		return "index";
 	}
 	
