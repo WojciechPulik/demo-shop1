12c53fef5f6b210e9ea539f8340a7f669bedae38
WojciechPulik <59091416+WojciechPulik@users.noreply.github.com>
2020-12-03 02:08:36 +0100

    open new tab for product picture

--- src/main/java/pl/wpulik/controller/PictureController.java
+++ src/main/java/pl/wpulik/controller/PictureController.java
@@ -2,13 +2,17 @@ package pl.wpulik.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import pl.wpulik.model.Picture;
+import pl.wpulik.model.Product;
 import pl.wpulik.service.PictureRepoService;
 import pl.wpulik.service.PictureService;
+import pl.wpulik.service.ProductRepoService;
 import pl.wpulik.service.ProductService;
 
 @Controller
@@ -16,11 +20,14 @@ public class PictureController {
 	
 	private PictureService pictureService;
 	private PictureRepoService pictureRepoService;
+	private ProductRepoService productRepoService;
 	
 	@Autowired
-	public PictureController(PictureService pictureService, PictureRepoService pictureRepoService) {
+	public PictureController(PictureService pictureService, PictureRepoService pictureRepoService,
+			ProductRepoService productRepoService) {
 		this.pictureService = pictureService;
 		this.pictureRepoService = pictureRepoService;
+		this.productRepoService = productRepoService;
 	}
 	
 	@PostMapping(value="/productimagesupdate", params="action=setMain")
@@ -33,6 +40,13 @@ public class PictureController {
 	public String removePictureFromProduct(@ModelAttribute Picture picture, @RequestParam Long productId) {
 		pictureService.removePictureFromProduct(picture.getId(), productId);
 		return String.format("redirect:/updateproduct/%d", productId);
+	}
+	
+	@GetMapping("/bigpicture")
+	public String displayPictureInNewWindow(@RequestParam String url, @RequestParam String productName,Model model) {
+		model.addAttribute("path", url);
+		model.addAttribute("title", productName);
+		return "bigpicture";
 	}
 	
 	
