933d88c864a4531782934d8731e5e033be67cdac
WojciechPulik <59091416+WojciechPulik@users.noreply.github.com>
2021-03-27 21:28:04 +0100

    admin authority implemented

--- src/main/java/pl/wpulik/security/SecurityConfig.java
+++ src/main/java/pl/wpulik/security/SecurityConfig.java
@@ -21,6 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception{
 		http
 		.authorizeRequests()
+		.antMatchers("/admin/**").hasAuthority("ADMIN")
 		.anyRequest().permitAll()
 		.and()
 		.formLogin()
